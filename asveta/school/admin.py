f23bcea7de8864ebaf8b3dc44405c904bbffcfe3
Kate Zinchenko <id.a1430@gmail.com>
2023-01-27 23:14:00 +0300

    second commit

--- asveta/school/admin.py
+++ asveta/school/admin.py
@@ -5,19 +5,25 @@ from .models import *
 
 
 class Student_Admin(admin.ModelAdmin):
-    list_display = ('user','amount_courses')
-    search_fields = ('user',)
+    list_display = ('name','amount_courses')
+    search_fields = ('name',)
 admin.site.register(Student, Student_Admin)
 
 
-class Subject_Admin(admin.ModelAdmin):
-    list_display = ('discipline','grade')
-    search_fields = ('grade','discipline')
-admin.site.register(Subject, Subject_Admin)
+# class Subject_Admin(admin.ModelAdmin):
+#     list_display = ('discipline','grade')
+#     search_fields = ('grade','discipline')
+# admin.site.register(Subject, Subject_Admin)
 
 
 class Teacher_Admin(admin.ModelAdmin):
-    list_display = ('user','education','work_experience','amount_courses')
-    search_fields = ('user',)
+    list_display = ('name','education','work_experience','amount_courses')
+    search_fields = ('name',)
 
 admin.site.register(Teacher, Teacher_Admin)
+
+class Course_Admin(admin.ModelAdmin):
+    list_display = ('subject','grade','teacher','time_lesson')
+    search_fields = ('subject','grade','teacher')
+
+admin.site.register(Course, Course_Admin)
--- asveta/school/models.py
+++ asveta/school/models.py
@@ -3,42 +3,57 @@ from django.contrib.auth.models import User
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    amount_courses = models.IntegerField(default=0, verbose_name='amount of courses attended')
-
-    class Meta:
-        verbose_name = 'Student'
-        verbose_name_plural = 'Students'
-
-
-class Subject:
-    discipline = models.CharField(max_length=20)
-    grade = models.IntegerField(default=0)
+    #user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, verbose_name='прозвішча, імя')
+    amount_courses = models.IntegerField(default=0, verbose_name='колькасць наведвальных курсаў')
+    courses = models.ManyToManyField("Course",verbose_name="урок")
 
     def __str__(self):
-        return f"{self.discipline} : {self.grade} grade"
+        return self.name
 
     class Meta:
-        verbose_name = 'Subject'
-        verbose_name_plural = 'Subjects'
+        verbose_name = 'Вучань'
+        verbose_name_plural = 'Вучнi'
 
 
-class Teacher:
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    education = models.CharField(blank=True)
-    work_experience = models.IntegerField(blank=True) # стаж
-    amount_courses = models.IntegerField(default=0, verbose_name='amount of courses conducted')
+# class Subject(models.Model):
+#     discipline = models.CharField(max_length=20)
+#     grade = models.IntegerField(default=0)
+#
+#     # def __str__(self):
+#     #     return f"{self.discipline} : {self.grade} grade"
+#
+#     class Meta:
+#         verbose_name = 'Subject'
+#         verbose_name_plural = 'Subjects'
+
+
+class Teacher(models.Model):
+    #user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, verbose_name='прозвішча, імя')
+    specialization = models.CharField(max_length=30, verbose_name='спецыялізацыя', blank=True)
+    education = models.CharField(max_length=20, blank=True,verbose_name='адукацыя')
+    work_experience = models.IntegerField(blank=True, verbose_name='стаж')
+    amount_courses = models.IntegerField(default=0, verbose_name='колькасць курсаў')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
-        verbose_name = 'Teacher'
-        verbose_name_plural = 'Teachers'
+        verbose_name = 'Настаўнік'
+        verbose_name_plural = 'Настаўнікі'
 
 
 class Course(models.Model):
-    teacher = models.OneToOneField(Teacher, on_delete=models.PROTECT)
-    subject = models.OneToOneField(Subject, on_delete=models.PROTECT)
-    time_lesson = models.TimeField();
+    subject = models.CharField(max_length=20, verbose_name='прадмет' )
+    grade = models.IntegerField(default=0, verbose_name='клас')
+    teacher = models.ForeignKey(Teacher, on_delete=models.PROTECT, verbose_name='настаўнік')
+    time_lesson = models.TimeField(verbose_name='час правядзення')
 
-class Course_Students(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.PROTECT)
-    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    def __str__(self):
+        return f"{self.subject} {self.grade} клас"
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
